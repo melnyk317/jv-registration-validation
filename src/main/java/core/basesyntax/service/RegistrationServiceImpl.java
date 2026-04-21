b613d3e14d4e400d6ed9ad554474188247130b61
darina_melnyk <me7darina2008@gmail.com>
2026-04-21 23:06:33 +0300

    Fix review comments

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,32 +5,34 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_DATA_LENGTH = 6;
+    private static final int MIN_AGE_ALLOWED = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
         if (user.getAge() == null) {
-            throw new NullDataException("Your age can`t be null");
+            throw new NullDataException("Your age can not be null");
         }
         if (user.getLogin() == null) {
-            throw new NullDataException("Your login can`t be null");
+            throw new NullDataException("Your login can not be null");
         }
         if (user.getPassword() == null) {
-            throw new NullDataException("Your login can`t be null");
+            throw new NullDataException("Your password can not be null");
         }
-        if (user.getAge() < 18) {
-            throw new InvalidAgeException("Your are too young, or your age is invalid");
+        if (user.getAge() < MIN_AGE_ALLOWED) {
+            throw new InvalidAgeException("Your are" + user.getAge() +
+                    " years old, but you need to be at least " + MIN_AGE_ALLOWED);
         }
-        if (user.getPassword().length() < 6) {
-            throw new InvalidDataLengthException("Login length must be at least 6");
-        }
-        if (user.getLogin().length() < 6) {
+        if (user.getPassword().length() < MIN_DATA_LENGTH) {
             throw new InvalidDataLengthException("Password length must be at least 6");
+        }
+        if (user.getLogin().length() < MIN_DATA_LENGTH) {
+            throw new InvalidDataLengthException("Login length must be at least 6");
         }
         if (storageDao.get(user.getLogin()) != null) {
             throw new UserExistException("This login is already taken");
         }
-        storageDao.add(user);
-        return user;
+        return storageDao.add(user);
     }
 }
--- src/test/java/core/basesyntax/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceImplTest.java
@@ -9,177 +9,241 @@ import core.basesyntax.model.User;
 import core.basesyntax.service.InvalidAgeException;
 import core.basesyntax.service.InvalidDataLengthException;
 import core.basesyntax.service.NullDataException;
-import core.basesyntax.service.RegistrationServiceImpl;
 import core.basesyntax.service.UserExistException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
-    private RegistrationServiceImpl registrationServiceImpl;
-    private User testUser;
+    private StorageDaoImpl storageDaoImpl = new StorageDaoImpl();
 
     @BeforeEach
     void setUp() {
-        registrationServiceImpl = new RegistrationServiceImpl();
-        testUser = new User();
+        storageDaoImpl = new StorageDaoImpl();
     }
 
     @Test
     void register_validAge_ok() {
+        User testUser = new User();
         testUser.setAge(20);
         testUser.setLogin("firstLogin");
         testUser.setPassword("thePassword");
-        User actual = registrationServiceImpl.register(testUser);
-        assertEquals(testUser, actual);
+        User actual = storageDaoImpl.add(testUser);
+        User expected = storageDaoImpl.add(testUser);
+        assertEquals(expected, actual);
     }
 
     @Test
     void register_exactly18Age_ok() {
+        User testUser = new User();
         testUser.setAge(18);
         testUser.setLogin("eidgthLogin");
         testUser.setPassword("thePassword");
-        User actual = registrationServiceImpl.register(testUser);
-        assertEquals(testUser, actual);
+        User actual = storageDaoImpl.add(testUser);
+        User expected = storageDaoImpl.add(testUser);
+        assertEquals(expected, actual);
     }
 
     @Test
     void register_underAge_notOk() {
+        User testUser = new User();
         testUser.setAge(7);
         testUser.setLogin("secondLogin");
         testUser.setPassword("thePassword");
         assertThrows(InvalidAgeException.class, () -> {
-            registrationServiceImpl.register(testUser);
+            storageDaoImpl.add(testUser);
         });
     }
 
     @Test
     void register_negativeAge_notOk() {
+        User testUser = new User();
         testUser.setAge(-7);
         testUser.setLogin("ninethLogin");
         testUser.setPassword("thePassword");
         assertThrows(InvalidAgeException.class, () -> {
-            registrationServiceImpl.register(testUser);
+            storageDaoImpl.add(testUser);
         });
     }
 
     @Test
     void register_nullAge_notOk() {
+        User testUser = new User();
         testUser.setAge(null);
         testUser.setLogin("thirdLogin");
         testUser.setPassword("thePassword");
         assertThrows(NullDataException.class, () -> {
-            registrationServiceImpl.register(testUser);
+            storageDaoImpl.add(testUser);
         });
     }
 
     @Test
     void register_validLogin_ok() {
+        User testUser = new User();
         testUser.setAge(20);
         testUser.setLogin("validLogin");
         testUser.setPassword("thePassword");
-        User actual = registrationServiceImpl.register(testUser);
-        assertEquals(testUser, actual);
+        User actual = storageDaoImpl.add(testUser);
+        User expected = storageDaoImpl.add(testUser);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void register_smallerValidLogin_ok() {
+        User testUser = new User();
+        testUser.setAge(20);
+        testUser.setLogin("qwdfhj");
+        testUser.setPassword("thePassword");
+        User actual = storageDaoImpl.add(testUser);
+        User expected = storageDaoImpl.add(testUser);
+        assertEquals(expected, actual);
     }
 
     @Test
     void register_nullLogin_notOk() {
+        User testUser = new User();
         testUser.setAge(20);
         testUser.setLogin(null);
         testUser.setPassword("thePassword");
         assertThrows(NullDataException.class, () -> {
-            registrationServiceImpl.register(testUser);
+            storageDaoImpl.add(testUser);
+        });
+    }
+
+    @Test
+    void register_3shortLogin_notOk() {
+        User testUser = new User();
+        testUser.setAge(20);
+        testUser.setLogin("");
+        testUser.setPassword("thePassword");
+        assertThrows(InvalidDataLengthException.class, () -> {
+            storageDaoImpl.add(testUser);
+        });
+    }
+
+    @Test
+    void register_2shortLogin_notOk() {
+        User testUser = new User();
+        testUser.setAge(20);
+        testUser.setLogin("vor");
+        testUser.setPassword("thePassword");
+        assertThrows(InvalidDataLengthException.class, () -> {
+            storageDaoImpl.add(testUser);
+        });
+    }
+
+    @Test
+    void register_1shortLogin_notOk() {
+        User testUser = new User();
+        testUser.setAge(20);
+        testUser.setLogin("gbmki");
+        testUser.setPassword("thePassword");
+        assertThrows(InvalidDataLengthException.class, () -> {
+            storageDaoImpl.add(testUser);
         });
     }
 
     @Test
     void register_shortLogin_notOk() {
+        User testUser = new User();
         testUser.setAge(20);
         testUser.setLogin("shotr");
         testUser.setPassword("thePassword");
         assertThrows(InvalidDataLengthException.class, () -> {
-            registrationServiceImpl.register(testUser);
+            storageDaoImpl.add(testUser);
         });
     }
 
     @Test
     void register_validPassword_ok() {
+        User testUser = new User();
         testUser.setAge(20);
         testUser.setLogin("fourthLogin");
         testUser.setPassword("validPassword");
-        User actual = registrationServiceImpl.register(testUser);
-        assertEquals(testUser, actual);
+        User actual = storageDaoImpl.add(testUser);
+        User expected = storageDaoImpl.add(testUser);
+        assertEquals(expected, actual);
     }
 
     @Test
     void register_smallerValidPassword_ok() {
+        User testUser = new User();
         testUser.setAge(20);
         testUser.setLogin("10thLogin");
         testUser.setPassword("asvkdn");
-        User actual = registrationServiceImpl.register(testUser);
-        assertEquals(testUser, actual);
+        User actual = storageDaoImpl.add(testUser);
+        User expected = storageDaoImpl.add(testUser);
+        assertEquals(expected, actual);
     }
 
     @Test
     void register_nullPassword_notOk() {
+        User testUser = new User();
         testUser.setAge(20);
         testUser.setLogin("fifthLogin");
         testUser.setPassword(null);
         assertThrows(NullDataException.class, () -> {
-            registrationServiceImpl.register(testUser);
+            storageDaoImpl.add(testUser);
         });
     }
 
     @Test
     void register_3shortPassword_notOk() {
+        User testUser = new User();
         testUser.setAge(20);
         testUser.setLogin("fifthLogin");
         testUser.setPassword("");
         assertThrows(InvalidDataLengthException.class, () -> {
-            registrationServiceImpl.register(testUser);
+            storageDaoImpl.add(testUser);
         });
     }
 
     @Test
     void register_2shortPassword_notOk() {
+        User testUser = new User();
         testUser.setAge(20);
         testUser.setLogin("fifthLogin");
         testUser.setPassword("mdg");
         assertThrows(InvalidDataLengthException.class, () -> {
-            registrationServiceImpl.register(testUser);
+            storageDaoImpl.add(testUser);
         });
     }
 
     @Test
     void register_1shortPassword_notOk() {
+        User testUser = new User();
         testUser.setAge(20);
         testUser.setLogin("fifthLogin");
         testUser.setPassword("dolbr");
         assertThrows(InvalidDataLengthException.class, () -> {
-            registrationServiceImpl.register(testUser);
+            storageDaoImpl.add(testUser);
         });
     }
 
     @Test
     void register_shortPassword_notOk() {
+        User testUser = new User();
         testUser.setAge(20);
         testUser.setLogin("sixthLogin");
         testUser.setPassword("short");
         assertThrows(InvalidDataLengthException.class, () -> {
-            registrationServiceImpl.register(testUser);
+            storageDaoImpl.add(testUser);
         });
     }
 
     @Test
     void register_userDoesNotExist_ok() {
+        User testUser = new User();
         testUser.setAge(20);
         testUser.setLogin("seventhLogin");
         testUser.setPassword("thePassword");
-        User actual = registrationServiceImpl.register(testUser);
-        assertEquals(testUser, actual);
+        User actual = storageDaoImpl.add(testUser);
+        User expected = storageDaoImpl.add(testUser);
+        assertEquals(expected, actual);
     }
 
     @Test
     void register_userWasAdded_ok() {
+        User testUser = new User();
         StorageDaoImpl storageDaoImpl = new StorageDaoImpl();
         String login = "12thLogin";
         testUser.setAge(20);
@@ -192,6 +256,7 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_userAlreadyExist_notOk() {
+        User testUser = new User();
         User existedUser = new User();
         existedUser.setAge(20);
         existedUser.setLogin("takenLogin");
@@ -201,7 +266,7 @@ class RegistrationServiceImplTest {
         testUser.setLogin("takenLogin");
         testUser.setPassword("thePassword");
         assertThrows(UserExistException.class, () -> {
-            registrationServiceImpl.register(testUser);
+            Storage.people.add(testUser);
         });
     }
 }
